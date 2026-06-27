688aef6c2f109ee552626ae46b3182d7b77794c6
Usuario <gustavoalbonicogoncalves@hotmail.com>
2026-06-27 13:38:09 -0300

    feat: SIGNAL | COMPUTED | EFFECT

---
+++ src/app/hello/hello.ts
@@ -0,0 +1,38 @@
+import { Component, computed, effect, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  imports: [],
+  templateUrl: './hello.html',
+  styleUrl: './hello.scss',
+})
+export class Hello {
+
+  protected title = "Modern Angular";
+
+  protected isDisabled = false;
+  
+  protected onClick(){
+    console.log("Button clicked");
+    this.isDisabled = !this.isDisabled;
+  }
+
+  protected count = signal(0);
+  protected doubleCount = computed(() => this.count() * 2);
+
+  private readonly countLog = effect(() => {
+    console.log("Count changed", this.count());
+  });
+
+  protected increateCounter() {
+    this.count.update(value => value + 1);
+  }
+
+  protected decreaseCounter() {
+    this.count.update(value => value - 1);
+  }
+
+  protected resetCounter() {
+    this.count.set(0);
+  }
+}
